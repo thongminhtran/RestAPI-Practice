f229f11bfb994efa8405a303729df69b35589089
b0648425 <thongminh.tran@aero.bombardier.com>
2024-06-11 15:56:48 -0600

    Update API endpoint to use local API

--- frontend/src/config.js
+++ frontend/src/config.js
@@ -1,5 +1,5 @@
 // Use the following URL for local development:
-export const API_URL = 'https://restapi-practice-it5p.onrender.com';
+export const API_URL = 'http://localhost:3001';
 
 // When deploying to production, replace the above URL with the following public API URL:
 // export const API_URL = 'https://restapi-practice-it5p.onrender.com';
